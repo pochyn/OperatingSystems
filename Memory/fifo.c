806c64aac42d18842efb6e601c05c13f93c31b29
pochyn <staspochyn@gmail.com>
2018-01-01 21:54:06 +0100

    Create fifo.c

---
+++ Memory/fifo.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include "pagetable.h"
+
+
+extern int memsize;
+
+extern int debug;
+
+extern struct frame *coremap;
+
+int last_used;
+
+/* Page to evict is chosen using the fifo algorithm.
+ * Returns the page frame number (which is also the index in the coremap)
+ * for the page that is to be evicted.
+ */
+int fifo_evict() {
+	int indx = last_used;
+	last_used += 1;
+	if (last_used == memsize - 1) {
+		last_used = 0;
+	}
+	return indx;
+}
+
+/* This function is called on each access to a page to update any information
+ * needed by the fifo algorithm.
+ * Input: The page table entry for the page that is being accessed.
+ */
+void fifo_ref(pgtbl_entry_t *p) {
+
+	return;
+}
+
+/* Initialize any data structures needed for this 
+ * replacement algorithm 
+ */
+void fifo_init() {
+	last_used = 0;
+}
